170e247146762bc4905746cc5fafdf05c0c8d75d
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-04-21 19:24:47 -0500

    dibujar la grafica con los calculos respectivos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const context = document.getElementById("data-set").getContext("2d");
+let line = new Chart(context,{});
+
+// message
+const message = document.getElementById("message")
 // values from the from
 const initialAmount = document.getElementById("initial-amount"),
       years = document.getElementById("investment-years"),
@@ -15,6 +20,9 @@ const data =[];
 const labels =[];
 
 function calculateGrowth(e){
+    data.length=0;
+    labels.length=0;
+    let growth =0;
     e.preventDefault();
     try {
         //values we're gonna need in our formula
@@ -28,13 +36,34 @@ function calculateGrowth(e){
             const final = initial * Math.pow(1 + ((interest / 100) / comp), comp * i);
 
             // send results
-            data.push(final);
+            data.push(toDecimal(final,2));
             labels.push("Years "+i);
+            growth =toDecimal(final,2);
         }
-        console.table(data);
+        //
+        message.innerText = `You will have this amount ${growth} after ${period} years`;
+        drawGraph();
     } catch (error) {
         console.error(error)
     }
+}
+
+function drawGraph(){
+    line.destroy();
+    // constructor
+    line = new Chart(context,{
+        type: 'line',
+        data:{
+            labels,
+            datasets: [{
+                label: "Compound",
+                data,
+                fill: true,
+                backgroundColor: "rgb(255, 182, 193)",
+                borderWidth: 3
+            }]
+        }
+    });
 }
 
 //  format the values with a given number of decimal places.
